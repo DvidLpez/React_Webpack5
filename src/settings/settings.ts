55395e3b84360500b164e3995ccbd1d7689fe544
dalopez <dalopez@mediamarkt.es>
2021-02-11 20:48:07 +0100

    Remove ignored files

--- src/settings/settings.ts
+++
@@ -1,8 +0,0 @@
-export const SETTINGS = {
-  API_GITHUB: {
-    URI: "https://api.github.com/graphql",
-    TOKEN: "360b16e91b8b76ab3896870ec327cf875594cbe7",
-    OWNER: "facebook",
-    NAME: "React",
-  },
-};
